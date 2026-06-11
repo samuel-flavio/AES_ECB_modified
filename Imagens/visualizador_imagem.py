d7715d9a891e23d64d48e10bae70175a67a7bbc8
samuel-flavio <samuel.flavio@alumni.usp.br>
2026-06-10 21:09:08 -0300

    Alterações para coleta de resultado do artigo

--- Imagens/visualizador_imagem.py
+++ Imagens/visualizador_imagem.py
@@ -42,6 +42,8 @@ def main():
     # visualizar_imagem_binaria('Imagens/VistaAereaSP.bin', largura=512, altura=512, colorida=True)
     # visualizar_imagem_binaria('Imagens/resultado_fpga_VistaAereaSP.bin', largura=512, altura=512, colorida=True)
 
-    visualizar_imagem_binaria('C:\\Xilinx\\Projetos\\TF\\Imagens\\resultado_fpga_ImagemTrabalhoModeloProprioPB.bin', largura=512, altura=512, colorida=False)
+    visualizar_imagem_binaria('C:\\Xilinx\\Projetos\\TF\\Imagens\\RESULTADO AES_COUNTER\\resultado_fpga_ImagemTrabalhoModeloProprio.bin', largura=512, altura=512, colorida=True)
+    visualizar_imagem_binaria('C:\\Xilinx\\Projetos\\TF\\Imagens\\RESULTADO AES_COUNTER\\resultado_fpga_VistaAereaSP.bin', largura=512, altura=512, colorida=True)
+    visualizar_imagem_binaria('C:\\Xilinx\\Projetos\\TF\\Imagens\\RESULTADO AES_COUNTER\\resultado_fpga_CisnePretoBranco.bin', largura=512, altura=512, colorida=False)
 if __name__ == "__main__":
     main()
